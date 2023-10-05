c4ebeb916f1c259c482ad1bab7ce6f4857dc8c80
orlyplaza <orly.plaza@valens-research.com>
2023-10-05 23:30:49 +0800

    Added a pyscopg package and setup databases

--- core/settings.py
+++ core/settings.py
@@ -94,10 +94,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pauleighdo',
+        'USER': 'pauleighdo_user',
+        'PASSWORD': 'wn9wvpFSCUFFY7YNLtCVWpbrPOAJ5JWI',
+        'HOST': 'dpg-ckfd2i6afg7c73ab8fq0-a.singapore-postgres.render.com',
+        'PORT': '5432',
     }
 }
 
